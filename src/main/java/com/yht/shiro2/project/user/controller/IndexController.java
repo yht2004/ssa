708be52f7a6e8b02f4aea600678361d52ab8a55e
yht2004 <1921959159@qq.com>
2019-11-07 18:20:17 +0800

    commit-5

--- src/main/java/com/yht/shiro2/project/user/controller/IndexController.java
+++ src/main/java/com/yht/shiro2/project/user/controller/IndexController.java
@@ -2,10 +2,14 @@ package com.yht.shiro2.project.user.controller;
 
 import com.yht.shiro2.framework.web.base.BaseController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class IndexController extends BaseController {
 
-
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
 }
