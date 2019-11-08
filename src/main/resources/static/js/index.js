0168d18fdc76cf186f8a30b542a50412a6eb6729
yht2004 <1921959159@qq.com>
2019-11-08 18:04:41 +0800

    commit-6

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,37 @@
 $(function () {
     $("#side-menu").metisMenu();
 })
+
+function SmoothlyMenu() {
+    if (!$('body').hasClass('mini-navbar')){
+        $('#side-menu').hide();
+        setTimeout(function () {
+            $('#side-menu').fadeIn(500);
+        },100)
+    }else if ($('body').hasClass('fixed-sidebar')){
+        $('#side-menu').hide();
+        setTimeout(function () {
+            $('#side-menu').fadeIn(500);
+        },300);
+    } else {
+        $('#side-menu').removeAttr('style');
+    }
+}
+
+function NavToggle(){
+    $('.navbar-minimlize').trigger('click');
+}
+
+/**
+ * iframe处理
+ */
+$(function () {
+
+    //通过遍历给菜单加上data-index属性
+    $(".menuItem").each(function (index) {
+        if (!$(this).attr('data-index'));
+        $(this).attr('data-index',index);
+    })
+})
+
+
